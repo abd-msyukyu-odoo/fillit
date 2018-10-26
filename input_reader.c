0adc9308a58b02c0c909ec6d0a5daeed56964801
Damien Abeloos <dabeloos@w-r10-p1.s19.be>
2018-10-26 18:55:20 +0200

    input_reader v1

--- input_reader.c
+++ input_reader.c
@@ -6,7 +6,7 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/25 15:25:13 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/10/26 18:02:13 by dabeloos         ###   ########.fr       */
+/*   Updated: 2018/10/26 18:55:02 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,23 +28,22 @@ char		check_tetro_read(char *tetro_read)
 	index_line = 0;
 	while (tetro_read[++i] != '\0')
 	{
-		if (tetro_read == '\n')
+		if ((i + 1) % (TETRO_SIZE + 1) == 0)
 		{
+			if (tetro_read[i] != '\n')
+				return (0);
 			if (index_line != TETRO_SIZE)
 				return (0);
 			index_line = 0;
-			continue ;
 		}
-		if (!is_valid_char(tetro_read[i]))
+		else if(!is_valid_char(tetro_read[i]))
 			return (0);
-		if (tetro_read[i] == '#' && count_hash == TETRO_SIZE)
+		if (tetro_read[i] == '#' && ++count_hash > TETRO_SIZE)
 			return (0);
-		else
-			++count_hash;
-		if (index_line == TETRO_SIZE)
+		if (++index_line > TETRO_SIZE)
 			return (0);
-		
 	}
+	return (1);
 }
 
 TETRO		*read_tetro(int fd, TETRO *tetro_box, int index)
@@ -54,8 +53,6 @@ TETRO		*read_tetro(int fd, TETRO *tetro_box, int index)
 	TETRO			current;
 
 	nread = read(fd, tetro_read, (TETRO_SIZE + 1) * TETRO_SIZE + 1);
-	if (nread == 0)
-		return ((TETRO *)ft_memalloc(sizeof(TETRO) * (index + 1)));
 	if (nread != (TETRO_SIZE + 1) * TETRO_SIZE + 1)
 		return (NULL);
 	tetro_read[nread] = '\0';
@@ -64,6 +61,17 @@ TETRO		*read_tetro(int fd, TETRO *tetro_box, int index)
 	current = flood_fill(ft_strsplit(tetro_read), '\n');
 	if (!current)
 		return (NULL);
+	nread = read(fd, tetro_read, 1);
+	if (nread == 0)
+		tetro_box = (TETRO *)ft_memalloc(sizeof(TETRO) * (index + 2));
+	else if (nread != 1 || tetro_read[0] != '\n')
+		return (NULL);
+	else
+		tetro_box = read_tetro(fd, tetro_box, index + 1);
+	if (tetro_box == NULL)
+		return (NULL);
+	tetro_box[index] = current;
+	return (tetro_box);
 }
 
 TETRO		*read_file(char *file)
@@ -73,5 +81,7 @@ TETRO		*read_file(char *file)
 	fd = open(file, O_RDONLY);
 	if (fd < 0)
 		return (NULL);
-	return (read_tetro(fd, tetro_box, 0));
+	tetro_box = read_tetro(fd, tetro_box, 0);
+	close(fd);
+	return (tetro_box);
 }
