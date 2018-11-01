e68c5496ecec585f6c2f221a394cda08e50c37a1
Rayan Hunders <rhunders@w-r8-p3.s19.be>
2018-11-01 19:34:56 +0100

    algo opti

--- solver.c
+++ solver.c
@@ -6,12 +6,13 @@
 /*   By: rhunders <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/28 17:57:12 by rhunders          #+#    #+#             */
-/*   Updated: 2018/11/01 13:19:03 by rhunders         ###   ########.fr       */
+/*   Updated: 2018/11/01 18:35:46 by rhunders         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 #include <stdio.h>
+#include <time.h>
 
 int			fill_pattern(MAP *map, TETRO *piece, int index, COORD point)
 {
@@ -21,6 +22,7 @@ int			fill_pattern(MAP *map, TETRO *piece, int index, COORD point)
 	i = 0;
 	point.x -= piece->footprint.x;
 	point.y -= piece->footprint.y;
+	piece->origin = point;
 	while (i < TETRO_SIZE)
 	{
 		pos.y = piece->pattern[i].y + point.y;
@@ -66,35 +68,39 @@ static int v_line(MAP *map, int nb_tetro, int y)
 	while (i < map->l_map)
 		if (map->board[y][i++] == '.')
 			count++;
+	if ((i = 0) || (y + 1 < map->l_map && map->space[y + 1]))
+		while (i < y)
+			map->tot += map->space[i++];
 	if ((map->tot + count) > diff)
 		return (0);
 	map->space[y] = count;
 	map->tot = 0;
-	if ((i = 0) || (y + 1 < map->l_map && map->space[y + 1]))
-		while (i < y)
-			map->tot += map->space[i++];
 	return (1);
 }
 
-static int	fillit(BOX *box, MAP *map, int index, int try)
+static int	fillit(BOX *box, MAP *map, int index, int try, int acc)
 {
 	COORD			point;
 	int				i;
+	int				i2;
 
+	i2 = 0;
 	i = 0;
 	if (index == box->nb_tetro)
-	{
-		while (i < box->nb_tetro)
-			if (!map->rest[i++])
-				return (0);
-		return (1);
-	}
-	if (try == 1 && init_map(map, &try, box) == -1)
+		while ((i2 || (i2 = index == box->nb_tetro)) && i <= box->nb_tetro)
+			if ((i == box->nb_tetro) || !map->rest[i++])
+				return (i == box->nb_tetro);
+	if ((i = 0) || (try == 1 && init_map(map, &try, box) == -1))
 		return (0);
 	if (map->rest[index])
-		return (fillit(box, map, index + 1, 0));
-	point.x = box->tetro_box[index]->footprint.x;
-	point.y = box->tetro_box[index]->footprint.y;
+	{
+		map->tot = 0;
+		while (i < map->l_map)
+			if (!v_line (map, box->nb_tetro, i++))
+				return (0);
+		return (fillit(box, map, index + 1, 0, acc + 1));
+	}
+	point = box->tetro_box[index]->footprint;
 	while (point.y < map->l_map)
 	{
 		while (point.x < map->l_map)
@@ -102,20 +108,11 @@ static int	fillit(BOX *box, MAP *map, int index, int try)
 			if (fill_pattern(map, box->tetro_box[index], index, point))
 			{
 				map->rest[index] = 1;
-				i = (try >= 2) ? try - 2 : index + 1;
-				//i = 0;
-				/*if ((try < 2 || (i == box->nb_tetro || map->rest[i])))
-				{
-					i = 0;
+				i = index + 1;
+				if ((index + 1 == box->nb_tetro || map->rest[index + 1]) && !(i = 0))
 					while (i < box->nb_tetro && map->rest[i])
 						i++;
-				}*/
-				//if (try >= 2)
-				//	if (v_line (map, box->nb_tetro, point.y - box->tetro_box[index]->footprint.y))
-						//return (0);
-			//	if (try == 2)
-			//		return ();
-				if (fillit(box, map, i, 0))
+				if (fillit(box, map, i, 0, acc+1))
 					return (1);
 				map->rest[index] = 0;
 				erase_pattern(map->board, *box->tetro_box[index], point);
@@ -123,19 +120,19 @@ static int	fillit(BOX *box, MAP *map, int index, int try)
 			point.x += 1;
 		}
 		point.x = box->tetro_box[index]->footprint.x;
-		if (!index && point.y++)
-			continue ;
 		if (v_line (map, box->nb_tetro, point.y - box->tetro_box[index]->footprint.y))
 			point.y += 1;
-		else if ((i = index + 1 == box->nb_tetro) || fillit(box, map, index + 1, index + 2))
-	  		//point.y = box->tetro_box[index]->footprint.y;
+		else if ((i = index + 1 == box->nb_tetro) || fillit(box, map, index + 1, 0, acc + 1))
 			return (!i);
 		else
 			break;
 	}
 	map->tot = 0;
-	return ((!index) ? fillit(box, map, 0, 1) : 0);
+	map->rest[index] = 0;
+	return ((!index) ? fillit(box, map, 0, 1, acc + 1) : 0);
 }
+
+
 
 int			main(int arc, char **argv)
 {
@@ -159,14 +156,17 @@ int			main(int arc, char **argv)
 		return (0);
 	while (i < box->nb_tetro || (i = 0))
 		map.rest[i++] = 0;
-	if (!(fillit(box, &map, 0, 1)))
+	if (!(fillit(box, &map, 0, 1, 0)))
 	{
 		ft_putendl("backtrack error");
 		return (0);
 	}
-	while (i < map.l_map)
+	while (i </*=*/ map.l_map)
 	{
-		ft_putendl(map.board[i]);
+		//if (i != map.l_map)
+			ft_putendl(map.board[i]);
+		//else
+		//	ft_putendl("finish");
 		i++;
 	}
 	return (0);
