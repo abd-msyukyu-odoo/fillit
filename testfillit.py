d7f5f59376fd98b00e94100b5560c06bdf48e56f
Damien Abeloos <dabeloos@w-r9-p1.s19.be>
2019-06-25 18:54:09 +0200

    modif_test

--- testfillit.py
+++ testfillit.py
@@ -2,7 +2,7 @@
 from __future__ import print_function
 import sys, random, os
 
-for i in range(1000):
+for i in range(100):
     n_tetro = random.randrange(1, 12)
     os.system("./random_tetriminos.py " + str(n_tetro) + "&& ./our_fillit ttmnfile > ourfillitlog" + str(i) + "&& ./fillit ttmnfile > fillitlog" + str(i) + "&& diff fillitlog" + str(i) + " ourfillitlog" + str(i) + " > diff" + str(i))
     os.system("echo -n" + str(i) + " && cat diff" + str(i))
